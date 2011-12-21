7caf8f5781b96e63533e305a02713736b24075f8
Bernie Pope <florbitous@gmail.com>
2011-12-21 18:43:03 +1100

    Refactored types into types module

--- Profiling/Linux/Perf/Types.hs
+++ Profiling/Linux/Perf/Types.hs
@@ -49,24 +49,37 @@ data Event =
    , ev_payload :: EventPayload
    }
 
+instance Pretty Event where
+   pretty ev@(Event {}) =
+      text "header: " <+> (pretty ev_header ev) $$
+      text "payload: " <+> (pretty ev_payload ev)
+
 data EventType -- perf_event_header->type
-  = EventType_0            -- unused
-  | PERF_RECORD_MMAP       -- 1
-  | PERF_RECORD_LOST       -- 2
-  | PERF_RECORD_COMM       -- 3
-  | PERF_RECORD_EXIT       -- 4
-  | PERF_RECORD_THROTTLE   -- 5
-  | PERF_RECORD_UNTHROTTLE -- 6
-  | PERF_RECORD_FORK       -- 7
-  | PERF_RECORD_READ       -- 8
-  | PERF_RECORD_SAMPLE     -- 9
+   = EventType_0            -- unused
+   | PERF_RECORD_MMAP       -- 1
+   | PERF_RECORD_LOST       -- 2
+   | PERF_RECORD_COMM       -- 3
+   | PERF_RECORD_EXIT       -- 4
+   | PERF_RECORD_THROTTLE   -- 5
+   | PERF_RECORD_UNTHROTTLE -- 6
+   | PERF_RECORD_FORK       -- 7
+   | PERF_RECORD_READ       -- 8
+   | PERF_RECORD_SAMPLE     -- 9
+   deriving (Eq,Show)
+
+instance Pretty EventType where
+   pretty = text . show
 
 data EventCPUMode -- a bitfield in perf_event_header->misc
-  = PERF_RECORD_CPUMODE_UNKNOWN -- 0
-  | PERF_RECORD_MISC_KERNEL     -- 1
-  | PERF_RECORD_MISC_USER       -- 2
-  | PERF_RECORD_MISC_HYPERVISOR -- 3
+   = PERF_RECORD_CPUMODE_UNKNOWN -- 0
+   | PERF_RECORD_MISC_KERNEL     -- 1
+   | PERF_RECORD_MISC_USER       -- 2
+   | PERF_RECORD_MISC_HYPERVISOR -- 3
+   deriving (Eq,Show)
 
+instance Pretty EventCPUMode where
+   pretty = text . show
+ 
 -- Corresponds with the perf_file_section struct in <perf source>/util/header.h
 data FileSection
   = FileSection {
